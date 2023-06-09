b67756a3ecb4325c91bcc51ef20f3c2c7e698a60
MMDIOUF <dioufmakhtar77@gmail.com>
2023-06-09 11:33:27 +0000

    rattrapage

---
+++ ratrrapage/src/main/java/ism/ratrrapage/controllers/InscriptionController.java
@@ -0,0 +1,36 @@
+package ism.ratrrapage.controllers;
+
+import ism.ratrrapage.entities.Classe;
+import ism.ratrrapage.entities.Etudiant;
+import ism.ratrrapage.entities.Inscription;
+import ism.ratrrapage.repository.ClasseRepository;
+import ism.ratrrapage.repository.EtudiantRepository;
+import ism.ratrrapage.repository.InscriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class InscriptionController {
+
+@Autowired
+    private InscriptionRepository inscriptionRepository;
+@Autowired
+    private EtudiantRepository etudiantRepository;
+@Autowired
+    private ClasseRepository classeRepository;
+
+
+    @GetMapping("/liste-inscriptions")
+    public String listerInscription(Model model){
+        List<Inscription> inscriptions=inscriptionRepository.findAll();
+        model.addAttribute("listeInscriptions",inscriptions);
+        return "inscription";
+    }
+}
---
+++ ratrrapage/src/main/java/ism/ratrrapage/entities/Inscription.java
@@ -0,0 +1,37 @@
+package ism.ratrrapage.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "inscriptions")
+@DiscriminatorValue(value = "Inscription")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Inscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "classe_id")
+    private Classe classe;
+
+    @ManyToOne
+    @JoinColumn(name = "etudiant_id")
+    private Etudiant etudiant;
+
+
+}
